f83cb0faf7e473498a1172e8e3af93a8213640e4
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-08-28 23:25:56 -0400

    folder structure setup

--- index.js
+++ index.js
@@ -9,3 +9,49 @@ const connection = mysql.createConnection({
     user: 'root',
     database: 'test'
   });
+
+
+
+  function chooseRole () {
+
+    inquirer.prompt([
+            {
+                  type: 'list',
+                   name: "whoNext",
+                   message: "Whats would you like to do",
+                   choices: [' view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role' ]
+               }
+           ]).then(ans => {
+               if(ans.whoNext === ' view all departments'){
+                viewDepartments()
+               }
+               else if(ans.whoNext === 'view all roles'){
+                   viewRoles()
+               }
+               else if(ans.whoNext === 'view all employees'){
+                viewEmployees()
+            }
+            else if(ans.whoNext === 'add a department'){
+               addDepartment()
+            }
+            else if(ans.whoNext === 'add a role'){
+                addRole()
+             }
+             else if(ans.whoNext === 'add an employee'){
+                addEmployee()
+             }
+             else if(ans.whoNext === 'update an employee role'){
+                updateEmployeeRole()
+             }
+
+               else {
+                    writeToFile()
+               } 
+                   
+               
+           })
+        }
+
+        function viewDepartments() {
+            inquirer.prompt()
+        }
